3593dd0bdff476bd7a56225e4b702990b53a6e95
StellaCea <stela.ceaicovscaia@hotmail.com>
2023-07-13 15:06:28 +0200

    created user-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,11 +19,13 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: UserProfileComponent}, 
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
@@ -33,7 +35,8 @@ const appRoutes: Routes = [
     UserRegistrationFormComponent,
     UserLoginFormComponent,
     MovieCardComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,67 @@
+import { formatDate } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+  user: any= {};
+  favoriteMovies: any[] = [];
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    private router: Router, 
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+//Get info about the user and favorite movies from the API
+  getUser(): void {
+    this.user = this.fetchApiData.getUser();
+    this.userData.Username = this.user.Username;
+    this.userData.Email = this.user.Email;
+    this.userData.Birthday = formatDate(this.user.Birthday, 'yyyy-mm-dd', 'en-US', 'UTC+0');
+
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.favoriteMovies = resp.filter((m: { _id: any; }) => this.user.FavoriteMovies.indexOf(m._id) >= 0);
+    });
+  }
+  //Update user data
+  updateUser(): void {
+    this.fetchApiData.editUser(this.userData).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result));
+
+      this.snackBar.open('User successfully updated', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  //delete the user
+  deleteUser(): void {
+    this.fetchApiData.deleteUser().subscribe((result) => {
+      localStorage.clear();
+      this.router.navigate(['welcome']);
+      this.snackBar.open('User successfully deleted', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+}
